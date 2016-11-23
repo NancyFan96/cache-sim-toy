770cc0d15d1b872033372064311e11e51caf8333
Nancy Fan <hmfnj1996@gmail.com>
2016-11-23 11:33:35 +0800

    Initial Commit

---
+++ CacheLab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CacheLab
+//
+//  Created by Nancy Fan on 16/11/23.
+//  Copyright © 2016年 Nancy Fan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
