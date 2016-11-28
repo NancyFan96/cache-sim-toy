e3387428e99b9de5a558de16b5af5df6d309898c
Nancy Fan <hmfnj1996@gmail.com>
2016-11-29 01:12:45 +0800

    2.0 version, real complementation for Lab 3-1

---
+++ cache-v2/memory.h
@@ -0,0 +1,27 @@
+#ifndef CACHE_MEMORY_H_
+#define CACHE_MEMORY_H_
+
+#include <stdint.h>
+#include "def.h"
+#include "storage.h"
+
+
+class Memory: public Storage {
+public:
+    Memory();
+    ~Memory(){};
+    
+    // Main access process
+    void HandleRequest(uint64_t addr, int bytes, int read,
+                       char *content, int &hit, int &time);
+    
+private:
+    // Memory implement
+    byte * cmem_zero;
+    
+    DISALLOW_COPY_AND_ASSIGN(Memory);
+};
+
+extern Memory c_mem;
+
+#endif //CACHE_MEMORY_H_
